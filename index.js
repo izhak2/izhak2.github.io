ee57e5595d7cfe0a27105a8cba6d9ceb2286e632
Izhak <izhak@genesis-labs.io>
2020-07-14 00:43:50 +0300

    updated

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-var express = require('express')
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
 var tex2max = require("tex2max")
-const converter = new tex2max();
 
-var app = express()
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-
-
-app.get(app.use(express.static(__dirname + '/')), function(req, res) {
-  res.render(__dirname + "test.html", {converter:converter});
+var hbs = exphbs.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo: function () { return 'FOO!'; },
+        bar: function () { return 'BAR!'; }
+    }
 });
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function (req, res, next) {
+    res.render('home', {
+        showTitle: true,
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+        // Override `foo` helper only for this rendering.
+        helpers: {
+            foo: function () { return 'foo.'; }
+        }
+    });
+});
+
+app.listen(3000);
