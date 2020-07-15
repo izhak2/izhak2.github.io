4029d619737bc653e921feaf863255905adcc328
Izhak <izhak@genesis-labs.io>
2020-07-15 21:23:52 +0300

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res, next) {
-    res.render('index', {'converter': converter});
+    res.render('index', {'converter': JSON.stringify(converter)});
 });
 
 app.listen(app.get('port'), function() {
