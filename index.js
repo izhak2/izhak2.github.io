9eba5a7769a5e7f8348d9ea8b8c52bcd04e6b2ce
Izhak <izhak@genesis-labs.io>
2020-07-15 21:15:20 +0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 var express = require('express');
 var tex2max = require("tex2max");
+const converter = new tex2max();
 
 var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res, next) {
-    res.render('index', {'tex2max': tex2max});
+    res.render('index', {'converter': converter});
 });
 
 app.listen(app.get('port'), function() {
