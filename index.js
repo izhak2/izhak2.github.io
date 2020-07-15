7274c57348840a8400c49d050e702c488ecdb160
Izhak <izhak@genesis-labs.io>
2020-07-15 20:59:12 +0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 
 var express = require('express');
 var tex2max = require("tex2max");
-var amdloader = require("amd-loader");
-const bodyParser = require('body-parser');
 
 var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
 
 app.get('/', function (req, res, next) {
-    res.render('index', {tex2max: JSON.stringify(tex2max)});
+    res.render('index', {tex2max: tex2max);
 });
 
 app.listen(app.get('port'), function() {
