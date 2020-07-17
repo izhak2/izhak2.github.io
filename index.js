1daa2386d27dd1e9b65da0da46a736a0406a0ec4
Izhak <izhak@genesis-labs.io>
2020-07-17 17:23:41 +0300

    update

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res, next) {
     res.render('index', {'converter': converter});
 });
-app.get('')
+
+app.post('/translate', function (req, res) {
+      var latex_data = JSON.parse( req.body.latex_data );
+      var maxima_data = converter.toMaxima(latex_data);
+      console.log( latex_data, maxima_data );
+      res.end( JSON.stringify(maxima_data));
+};
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
